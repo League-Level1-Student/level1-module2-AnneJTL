485105ad640f23de8a8ef14aa2d457d398bc9bbc
AnneJTL <anne.chatin@jointheleague.org>
2018-09-17 16:33:48 -0700

    Platypus

---
+++ src/PlatypusRunner.java
@@ -0,0 +1,12 @@
+
+public class PlatypusRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Platypus ornithorinque = new Platypus("Ornitoto");
+		
+		ornithorinque.sayHi();
+
+	}
+
+}
